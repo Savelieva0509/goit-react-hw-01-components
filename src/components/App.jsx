66a2fdd7634bd9ae24eb68ff6a4587745019909d
Babenko Liudmyla <savelieva0509@gmail.com>
2022-11-09 17:33:44 +0200

    компонент profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import Profile from 'components/profile';
+import user from 'data/user.json';
+
+
 export const App = () => {
+  const { username, tag, location, avatar, stats } = user;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+    username = {username}
+    tag = {tag}
+    location = {location}
+    avatar = {avatar}
+    stats = {stats} 
+  />
     </div>
-  );
+  )
 };
+
